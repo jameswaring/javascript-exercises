9b87899838898980179f78b4e2f9d77c6cf24739
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-05-10 21:33:59 -0400

    first two tasks completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, number) {
+    if(number < 0){
+        return "ERROR"
+    }
+    let result = ""
+      for(let i = 0; i < number; i++){
+        result += word
+      }
+    return result
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
