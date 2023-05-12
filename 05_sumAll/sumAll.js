15be61ec0cdc363821c16c14d6b47f85ea450c29
James Waring <jameswaring@WAS-C02YD182JGH5.local>
2023-05-12 13:21:11 -0400

    completed sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0 || typeof a != "number" || typeof b != "number"){
+      return 'ERROR'
+    }
+    if(a > b){
+        let c = a
+        a = b
+        b = c
+      }
+    let total = 0
+    for(let i=a; i<b+1; i++){
+      total += i;
+    }
+    return total
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
