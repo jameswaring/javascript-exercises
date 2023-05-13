512cb27bca0d34eeb94cb44f14737f7d45975480
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-05-13 12:31:11 -0400

    completed up to Palindromes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = 5/9*(temp-32)
+  if(celcius%1 == 0){
+    return celcius
+  }
+  celcius = +celcius.toFixed(1)
+  return celcius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let far = temp*(9/5) + 32
+  if(far%1 == 0){
+    return far
+  }
+  far = +far.toFixed(1)
+  return far
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
+const palindromes = function(word){
+  let reversed = word.split("").reverse().join("");
+  reversed = reversed.replace(/[^a-z0-9]/gi, '');
+  reversed = reversed.replace(/\s/g, '');
+  reversed = reversed.toLowerCase()
+  word = word.toLowerCase()
+  word = word.replace(/[^a-z0-9]/gi, '');
+  word = word.replace(/\s/g, '');
+  return word == reversed
+}
 
 // Do not edit below this line
 module.exports = palindromes;
