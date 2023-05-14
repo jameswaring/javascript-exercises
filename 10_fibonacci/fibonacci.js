271559f197280ba508e73e96e8bc67db7b556879
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-05-14 13:46:55 -0400

    completed up to find the oldest

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(position) {
+    if(position < 0){
+        return 'OOPS'
+    }
+    position = parseInt(position)
+    let a = 0
+    let b = 1
+    let c
+    for(let i = 0; i<position; i++){
+      c = a+b
+      a = b
+      b = c
+    }
+    return(a)
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let winner
+    let distance = 0
+    let birth
+    let death
+    for(let i = 0; i < people.length; i++){
+      if(!("yearOfDeath" in people[i])){
+        console.log(new Date().getFullYear())
+        death = new Date().getFullYear()
+        birth = people[i].yearOfBirth
+      }
+      else{
+        birth = people[i].yearOfBirth
+        death = people[i].yearOfDeath
+      }
+      if((death - birth) > distance){
+        distance = (death - birth)
+        winner = people[i]
+      }
+    }
+    return winner
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
